c0db97a51a49ea68ef49b27d7d56fe96d3ef13e7
George <g9c9@outlook.com>
2021-07-15 19:47:33 -0700

    Using React Testing Library

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-//https://api.github.com/users/g9c9
-
 function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;
-
   return (
     <div>
-      <h1>{data.name}</h1>
-      <img alt={data.login} src={data.avatar_url} />
+      <h1>Hello React Testing Library</h1>
     </div>
   );
 }
